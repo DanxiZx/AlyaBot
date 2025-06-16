201b0d350441e7ef3f48ae79b24096b566b1e3a3
𝙈𝙇𝘽𝙏-𝙭𝙯 <diegolesimarcelino@gmail.com>
2025-06-16 12:02:02 -0400

    Actualizar auto-personalize.js

--- plugins/auto-personalize.js
+++ plugins/auto-personalize.js
@@ -5,7 +5,7 @@ const filePath = './personalize.json';
 // Datos predeterminados que se agregarán al archivo si no existe
 const defaultData = {
     default: {
-        botName: "𝑴𝑨𝑫𝑨𝑹𝑨-𝑼𝑪𝑯𝑰𝑯𝑨-𝑩𝑶𝑻",
+        botName: "𝘼𝙡𝙮𝙖𝙆𝙪𝙟𝙤𝙪-𝘽𝙤𝙩 💗",
         currency: "yenes",
         videos: [
             "https://files.catbox.moe/wp0tra.mp4",
